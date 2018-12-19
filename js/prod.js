10c7be37ee1eda2670e8f27ea55950ea784b1076
Elizabeth <andikoelizabeth@gmail.com>
2018-12-19 17:23:18 +0300

    modified users page

---
+++ js/prod.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#eq, #vh, #vn").show();
+
+   })
+   $(document).ready(function(){
+   $("#eq").click(function(){
+     $("#equipment").show();
+        $("#vn,#vh,#eq") . show();
+   })
+})
+    $(document).ready(function(){
+   $("#vh").click(function(){
+     $("#vehicle").show();
+        $("#vn,#vh,#eq") . show();
+   })
+})
+    $(document).ready(function(){
+   $("#vn").click(function(){
+     $("#venue").show();
+        $("#vn,#vh,#eq") . show();
+   })
+})
+
+
+
+   $(document).ready(function(){
+    $("#eq, #vh,#vn").show();
+
+   })
+   $(document).ready(function(){
+   $("#vn").click(function(){
+     $("#venue").show();
+        $("#equipment,#vehicle") . hide();
+   })
+})
+
+   $(document).ready(function(){
+   $("#vh").click(function(){
+     $("#vehicle").show();
+        $("#venue,#equipment") . hide();
+   })
+})
+    $(document).ready(function(){
+   $("#eq").click(function(){
+     $("#equipment").show();
+        $("#vehicle,#venue") . hide();
+   })
+})
