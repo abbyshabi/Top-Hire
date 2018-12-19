e4f1d68deb539bcf00ef071883371b117bcf25ae
joy <wendokingoo@gmail.com>
2018-12-19 19:02:14 +0300

    equipment linked to equipment-view update

--- js/view.js
+++ js/view.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#test1").click(function(){
+  $(".views").click(function(){
 	console.log("test passing");
       window.location.href = "equipment-view.html";
   });
